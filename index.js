a2ab2252e76935e17a473f2affbc968493a2851b
Wanjiku <wanjikukariuki03@gmail.com>
2020-01-10 10:07:10 +0300

    created skeleton connection to server

---
+++ index.js
@@ -0,0 +1,29 @@
+//I think since this file is dependent on so much it should be in the detail/outermost level.
+
+const express = require('express');
+const dotenv = require('dotenv');
+const MongoClient = require('mongodb').MongoClient;
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'})
+dotenv.config()
+const app = express()
+
+
+app.post('/instagramnode', async(req,res)=>{
+    var request = req.body
+    //display here ya cursor to find all pictures.
+    
+    //returning on the server the result
+    res.send(request)
+
+})
+
+//exiting the server
+process.on('SIGINT', () => {console.log('Exited server');process.exit(); })
+
+
+//initiating the server
+var port = 3000
+var server = app.listen(port, ()=>{
+    console.log('listening');
+});
